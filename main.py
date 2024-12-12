5fefec297ff78ed4b799b3af452549748805001a
DurveshDhake <109057181+DurveshDhake@users.noreply.github.com>
2024-12-12 13:59:23 +0530

    Update TensorFlow version and runtime

--- main.py
+++ main.py
@@ -57,6 +57,6 @@ def predict(input_data: PredictionInput):
     
     return {"predictions": labels}
 
-if _name_ == "_main_":
+if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
